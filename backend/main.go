0adc520927c76178ae9ed042424ce11450319b8f
Marko Pulkkinen <marko.pulkkinen@protacon.com>
2018-09-22 13:44:14 +0300

    Test api

--- backend/main.go
+++ backend/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"time"
+	"io/ioutil"
 	"net/http"
-
 	"google.golang.org/appengine"
 )
 
@@ -16,12 +17,12 @@ func main() {
 
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/test", TodoIndex)
-
+	http.HandleFunc("/api/test", TestApi)
 	appengine.Main()
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome!")
+	fmt.Fprintln(w, "Welcome1!")
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
@@ -29,4 +30,15 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(test)
+}
+
+func TestApi(w http.ResponseWriter, r *http.Request) {
+	var netClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
+	response, _ := netClient.Get("https://nimiq.mopsus.com/api/quick-stats")
+	data, _ := ioutil.ReadAll(response.Body)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, string(data))
 }
